8b131ad027e9ae1f673fd2997234cd59d29989ce
gizemabes <abes.gizem@gmail.com>
2022-10-20 18:29:49 +0300

    first commit

---
+++ hafta_4/bir.py
@@ -0,0 +1,6 @@
+
+#SORU:kendisine gönderilen sayılardan sadece palindrom olanları toplayan diğerlerini de bu toplamdan çıkaran ve geri döndüren fonksiyonu yazınız
+
+def palindrom (*sayi):
+    toplam=0
+    
